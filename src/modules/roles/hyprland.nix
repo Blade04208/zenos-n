62428c51b2ffe3ef1d4596476f46cc49e58aa9cb
blade0 <blade0@blade0.net>
2026-04-26 15:49:35 +0100

    8 grand dad

--- src/modules/roles/hyprland.nix
+++ src/modules/roles/hyprland.nix
@@ -1,8 +1,50 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 {
+
   programs.hyprland = {
     enable = true;
     withUWSM = false;
     xwayland.enable = true; # Xwayland can be disabled.
   };
+  environment.systemPackages = with pkgs; [
+    kitty
+    ironbar
+  ];
+  # programs.ironbar = {
+  #   enable = false;
+  #   systemd = true;
+  #   config = {
+  #     # An example:
+  #     monitors = {
+  #       DP-1 = {
+  #         anchor_to_edges = true;
+  #         position = "top";
+  #         height = 16;
+  #         start = [
+  #           { type = "clock"; }
+  #         ];
+  #         end = [
+  #           {
+  #             type = "tray";
+  #             icon_size = 16;
+  #           }
+  #         ];
+  #       };
+  #     };
+  #   };
+  #   style = /* css */ ''
+  #     /* An example */
+  #     * {
+  #       font-family: Noto Sans Nerd Font, sans-serif;
+  #       font-size: 16px;
+  #       border: none;
+  #       border-radius: 0;
+  #     }
+  #   '';
+  #   package = inputs.ironbar;
+  #   features = [
+  #     "feature"
+  #     "another_feature"
+  #   ];
+  # };
 }
