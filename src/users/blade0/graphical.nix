2c3663ae1d85133b24a639125c4e5da6dc7557b2
blade0 <blade0@blade0.net>
2026-05-03 12:45:30 +0100

    11 grand dad

--- src/users/blade0/graphical.nix
+++ src/users/blade0/graphical.nix
@@ -22,6 +22,7 @@
         telegram-desktop
         # [P13.D] Ensure formatter is available for the LSP
         nixfmt-rfc-style
+        vesktop
       ];
 
       # [USER SPECIFIC] VS Code Overrides
@@ -46,99 +47,6 @@
           StartupWMClass = "vesktop";
           Keywords = "discord;vencord;vesktop;";
         };
-      };
-
-      programs.nixcord = {
-        enable = true;
-
-        # [SWITCHED] Using Vesktop instead of official Discord
-        # Better Wayland support, screen sharing with audio, and performance.
-
-        discord.enable = false;
-        vesktop = {
-          enable = true;
-          autoscroll.enable = true;
-          state = {
-            "firstLaunch" = false;
-            "windowBounds" = {
-              "x" = 0;
-              "y" = 0;
-              "width" = 0;
-              "height" = 0;
-            };
-          };
-          settings = {
-            # "hardwareVideoAcceleration" = true;
-            "customTitleBar" = true;
-            "staticTitle" = false;
-          };
-        };
-
-        config = {
-          useQuickCss = true;
-          disableMinSize = true;
-
-          # [FIX] Load themes into the menu as well, just in case QuickCSS fails.
-          themeLinks = [
-            "https://codeberg.org/ridge/Discord-Adblock/raw/branch/main/discord-adblock.css"
-            "https://raw.githubusercontent.com/ricewind012/discord-gnome-theme/master/gnome.theme.css"
-            # ↑ broken, hopefully replaced by gord soon
-          ];
-
-          # Global Vencord Settings
-          enabledThemes = [
-            "discord-adblock.css"
-            "gnome.theme.css"
-          ];
-
-          # Highly Practical Plugin Configuration [p13.9 focus]
-          plugins = {
-            # Essentials
-            fakeNitro = {
-              enable = true;
-              transformEmojis = true;
-            };
-
-            themeAttributes.enable = true;
-
-            # UI Improvements
-            betterFolders = {
-              enable = false;
-              sidebar = false;
-              sidebarAnim = true;
-
-            };
-            memberCount.enable = true;
-            showHiddenThings.enable = true;
-
-            # [REMOVED] 'noMinSize' is not supported by current Nixcord version.
-            # Enable this manually in Vencord settings -> Plugins if needed.
-
-            # Privacy & Utility
-            callTimer.enable = true;
-            ClearURLs.enable = true;
-            CopyUserURLs.enable = true;
-
-            # Performance/Fixes
-            # vencordToolbox.enable = true;
-            webKeybinds.enable = true;
-
-            # [NOTE] Often redundant on Vesktop (it has native fixes), but harmless to keep.
-            webScreenShareFixes.enable = true;
-
-            youtubeAdblock.enable = true;
-          };
-        };
-
-        # [FIX] Added quotes to URLs to ensure CSS validity
-        quickCss = ''
-          .bar_c38106:nth-last-child(3) {
-              margin-top: 0px;
-          }
-          :root {
-              --chat-header-right-padding: calc(var(--titlebar-right-spacing) + var(--icon-button-size) * 2 + var(--spacing));
-          }
-        '';
       };
     };
 }
