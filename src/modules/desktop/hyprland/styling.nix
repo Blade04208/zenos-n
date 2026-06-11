7ba96a8b10f279d2a294dd55d9a60de26bb4a5c5
blade0 <blade0@blade0.net>
2026-06-11 14:00:37 +0100

    21 grand dad

--- src/modules/desktop/hyprland/styling.nix
+++ src/modules/desktop/hyprland/styling.nix
@@ -63,68 +63,9 @@
         #   "${pkgs.adw-gtk3}/share/themes/adw-gtk3-dark";
         # home.file.".local/share/icons/Adwaita-hacks".source = "${iconPkg}/share/icons/Adwaita-hacks";
 
-        xdg.configFile."gtk-3.0/gtk.css".text = ''
-                   window.background {
-          background: linear-gradient(180deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.125) 109.52%) , linear-gradient(rgba(81,81,81,0.3), rgba(81,81,81,0.3)), rgba(40,40,40,0.8);
-           /*   border: 1px solid rgba(0, 0, 0, 0.4); */
-              box-shadow: inset 0px 0px 0px 1px rgba(255, 255, 255, 0.077);
-          border-radius: 10px;
-          }
+        xdg.configFile."gtk-4.0/gtk.css".source = ./gtk.css;
+        xdg.configFile."gtk-3.0/gtk.css".source = ./gtk.css;
 
-          headerbar, header, stack, .view {
-          background: transparent;
-          }
-          headerbar {
-          box-shadow: none;
-          }
-
-          notebook {
-          background: rgba(0,0,0,0.4);
-          border-radius: 10px 0px 0px 0px;
-          border: 1px solid rgba(0, 0, 0, 0.2);
-              box-shadow: inset 0px 4px 8px -2px rgba(0,0,0,0.2);
-          }
-          .sidebar {
-          border: none;
-          }
-
-          menubutton button, .image-button:not(.close):not(.minimize):not(.maximize), .text-button, combobox {
-          background: linear-gradient(180deg, rgba(0, 0, 0, 0.176364), rgba(0, 0, 0, 0.128) 20%, rgba(0, 0, 0, 0.239453) 40%, rgba(0, 0, 0, 0.4) 65%, rgba(0, 0, 0, 0.4) 75%, rgba(0, 0, 0, 0.28));
-          border: 1px solid rgba(7,7,7,0.7);
-          border-radius: 5px;
-                }
-        '';
-        xdg.configFile."gtk-4.0/gtk.css".text = ''
-                   window.background {
-          background: linear-gradient(180deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.125) 109.52%) , linear-gradient(rgba(81,81,81,0.3), rgba(81,81,81,0.3)), rgba(40,40,40,0.8);
-           /*   border: 1px solid rgba(0, 0, 0, 0.4); */
-              box-shadow: inset 0px 0px 0px 1px rgba(255, 255, 255, 0.077);
-          border-radius: 10px;
-          }
-
-          headerbar, header, stack, .view {
-          background: transparent;
-          }
-          headerbar {
-          box-shadow: none;
-          }
-
-          notebook {
-          background: rgba(0,0,0,0.4);
-          border-radius: 10px 0px 0px 0px;
-          border: 1px solid rgba(0, 0, 0, 0.2);
-              box-shadow: inset 0px 4px 8px -2px rgba(0,0,0,0.2);
-          }
-          .sidebar {
-          border: none;
-          }
-
-          menubutton button, .image-button:not(.close):not(.minimize):not(.maximize), .text-button, combobox {
-          background: linear-gradient(180deg, rgba(0, 0, 0, 0.176364), rgba(0, 0, 0, 0.128) 20%, rgba(0, 0, 0, 0.239453) 40%, rgba(0, 0, 0, 0.4) 65%, rgba(0, 0, 0, 0.4) 75%, rgba(0, 0, 0, 0.28));
-          border: 1px solid rgba(7,7,7,0.7);
-          border-radius: 5px;
-                }
-        '';
       }
     )
   ];
